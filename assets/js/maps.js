d506a1728db21e43de6c5ce74f360df27206355e
adowlin <dowlingalison@gmail.com>
2021-02-26 15:28:30 +0000

    integrated Google Maps with index.html

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: {
+            lat: 53.2734,
+            lng: -7.77832031
+        }
+    });
+}
