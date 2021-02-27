77b43c01336cbf3f1cfe1e19613a4d5bd1f16263
adowlin <dowlingalison@gmail.com>
2021-02-27 23:24:43 +0000

    added dark styled map to Google Maps & linked to existing dark mode toggle

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,117 @@
 /* JS for map search functionality adapted from Google Developer Docs: https://developers.google.com/maps/documentation/javascript/examples/places-searchbox#maps_places_searchbox-html */
 function initAutocomplete() {
+    const styledMapType = new google.maps.StyledMapType(
+        [
+        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+        {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+        },
+        {
+            featureType: "poi",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+        },
+        {
+            featureType: "poi.park",
+            elementType: "geometry",
+            stylers: [{ color: "#263c3f" }],
+        },
+        {
+            featureType: "poi.park",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#6b9a76" }],
+        },
+        {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [{ color: "#38414e" }],
+        },
+        {
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#212a37" }],
+        },
+        {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#9ca5b3" }],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry",
+            stylers: [{ color: "#746855" }],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#1f2835" }],
+        },
+        {
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#f3d19c" }],
+        },
+        {
+            featureType: "transit",
+            elementType: "geometry",
+            stylers: [{ color: "#2f3948" }],
+        },
+        {
+            featureType: "transit.station",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
+        },
+        {
+            featureType: "water",
+            elementType: "geometry",
+            stylers: [{ color: "#17263c" }],
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#515c6d" }],
+        },
+        {
+            featureType: "water",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#17263c" }],
+        },
+        ],
+        { name: "Night Map" }
+    );
+    
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 51.89997191094052, lng: -8.470630645751955 },
         zoom: 18,
-        mapTypeId: "roadmap",
+        mapTypeControlOptions: {
+            mapTypeIds: ["roadmap", "styled_map"],
+        },
     });
+    //Associate the styled map with the MapTypeId.
+    map.mapTypes.set("styled_map", styledMapType);
+
+    //Toggle darkmode map by changing map mapTypeId
+    function darkMode(event) {
+        let body = document.body;
+        body.classList.toggle("body-dark-mode");
+        let darkModeStatus = body.classList.contains("body-dark-mode");
+        if (darkModeStatus) {
+            map.setMapTypeId("styled_map");
+        } else {
+            map.setMapTypeId("roadmap");
+        };
+    }
+    let darkModeSwitch1 = document.getElementById("dark-mode-slider");
+    darkModeSwitch1.addEventListener('click', darkMode);
+    let darkModeSwitch2 = document.getElementsByClassName("dark-mode-slider")[0];
+    darkModeSwitch2.addEventListener('click', darkMode);
+
     // Create the search box and link it to the UI element.
     const input = document.getElementById("map-search");
     const searchBox = new google.maps.places.SearchBox(input);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     
     function darkMode(event) {
         let body = document.body;
         body.classList.toggle("body-dark-mode");
+        let darkModeStatus = body.classList.contains("body-dark-mode");
+        if (darkModeStatus) {
+            map.setMapTypeId("styled_map");
+        } else {
+            map.setMapTypeId("roadmap");
+        };
     }
 
     let darkModeSwitch1 = document.getElementById("dark-mode-slider");
@@ -10,4 +16,4 @@ $(document).ready(function() {
 
     let darkModeSwitch2 = document.getElementsByClassName("dark-mode-slider")[0];
     darkModeSwitch2.addEventListener('click', darkMode);
-});
+});*/
