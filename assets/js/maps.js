e1a14f6114334b6dad26f95bef29e8fe0bbdb4d2
adowlin <dowlingalison@gmail.com>
2021-03-03 22:23:16 +0000

    added js to create list from nearbySearch results

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -164,16 +164,66 @@ function codeLocation() {
                 radius: '500',
                 type: [searchType]
             };
+            // Create the places service.
+            const service = new google.maps.places.PlacesService(map);
+            let getNextPage;
+            const moreButton = document.getElementById("more");
+
+            moreButton.onclick = function () {
+                moreButton.disabled = true;
+
+                if (getNextPage) {
+                    getNextPage();
+                }
+            };
             // perform a nearby search
-            service = new google.maps.places.PlacesService(map);
-            service.nearbySearch(request, callback);
+            service.nearbySearch(request, (results, status, pagination) => {
+                if (status !== "OK" || !results) return;
+                addPlaces(results, map);
+                moreButton.disabled = !pagination || !pagination.hasNextPage;
+
+                if (pagination && pagination.hasNextPage) {
+                    getNextPage = () => {
+                        // Note: nextPage will call the same handler function as the initial call
+                        pagination.nextPage();
+                    };
+                }
+            });
         } else {
             alert('Location provided is not supported. Please provide a location in Ireland.');
         }
     });
 }
 
-// adds Places request results to map-results-wrapper div (currently returns the objects, need to parse)
+function addPlaces(places, map) {
+    const placesList = document.getElementById("places");
+
+    for (const place of places) {
+        if (place.geometry && place.geometry.location) {
+            const image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25),
+            };
+            new google.maps.Marker({
+                map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location,
+            });
+            const li = document.createElement("li");
+            li.textContent = place.name;
+            placesList.appendChild(li);
+            li.addEventListener("click", () => {
+                map.setCenter(place.geometry.location);
+            });
+        }
+    }
+}
+
+/* adds Places request results to map-results-wrapper div (currently returns the objects, need to parse)
 function callback(results, status) {
     let searchResultsArea = document.getElementById('map-results-wrapper');
     if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -183,4 +233,4 @@ function callback(results, status) {
             //searchResultsArea.innerHTML = (results[i]);
         }
     }
-}
+}*/
